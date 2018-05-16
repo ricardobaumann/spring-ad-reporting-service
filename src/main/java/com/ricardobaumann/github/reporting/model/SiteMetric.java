c89b50f7818e65beeea3399e6a8ac100300e2e2c
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2018-05-16 22:54:55 +0200

    Data loader happy path specs

--- src/main/java/com/ricardobaumann/github/reporting/model/SiteMetric.java
+++ src/main/java/com/ricardobaumann/github/reporting/model/SiteMetric.java
@@ -1,14 +1,17 @@
 package com.ricardobaumann.github.reporting.model;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class SiteMetric {
 
     @Id
+    @GeneratedValue
     private Long id;
 
     private String site;
@@ -22,6 +25,26 @@ public class SiteMetric {
     private Integer conversions;
 
     private BigDecimal revenue;
+
+    private BigDecimal ctr;
+
+    private BigDecimal cr;
+
+    private BigDecimal fillRate;
+
+    private BigDecimal eCpm;
+
+    public SiteMetric(String site, Integer requests, Integer impressions, Integer clicks, Integer conversions, BigDecimal revenue) {
+        this.site = site;
+        this.requests = requests;
+        this.impressions = impressions;
+        this.clicks = clicks;
+        this.conversions = conversions;
+        this.revenue = revenue;
+    }
+
+    public SiteMetric() {
+    }
 
     public Long getId() {
         return id;
@@ -77,5 +100,78 @@ public class SiteMetric {
 
     public void setRevenue(BigDecimal revenue) {
         this.revenue = revenue;
+    }
+
+    public BigDecimal getCtr() {
+        return ctr;
+    }
+
+    public void setCtr(BigDecimal ctr) {
+        this.ctr = ctr;
+    }
+
+    public BigDecimal getCr() {
+        return cr;
+    }
+
+    public void setCr(BigDecimal cr) {
+        this.cr = cr;
+    }
+
+    public BigDecimal getFillRate() {
+        return fillRate;
+    }
+
+    public void setFillRate(BigDecimal fillRate) {
+        this.fillRate = fillRate;
+    }
+
+    public BigDecimal geteCpm() {
+        return eCpm;
+    }
+
+    public void seteCpm(BigDecimal eCpm) {
+        this.eCpm = eCpm;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SiteMetric that = (SiteMetric) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(site, that.site) &&
+                Objects.equals(requests, that.requests) &&
+                Objects.equals(impressions, that.impressions) &&
+                Objects.equals(clicks, that.clicks) &&
+                Objects.equals(conversions, that.conversions) &&
+                Objects.equals(revenue, that.revenue);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, site, requests, impressions, clicks, conversions, revenue);
+    }
+
+    @Override
+    public String toString() {
+        return "SiteMetric{" +
+                "id=" + id +
+                ", site='" + site + '\'' +
+                ", requests=" + requests +
+                ", impressions=" + impressions +
+                ", clicks=" + clicks +
+                ", conversions=" + conversions +
+                ", revenue=" + revenue +
+                ", ctr=" + ctr +
+                ", cr=" + cr +
+                ", fillRate=" + fillRate +
+                ", eCpm=" + eCpm +
+                '}';
     }
 }
--- src/main/java/com/ricardobaumann/github/reporting/service/DataLoader.java
+++ src/main/java/com/ricardobaumann/github/reporting/service/DataLoader.java
@@ -1,5 +1,6 @@
 package com.ricardobaumann.github.reporting.service;
 
+import com.ricardobaumann.github.reporting.model.SiteMetric;
 import com.ricardobaumann.github.reporting.repo.SiteMetricRepo;
 
 import org.apache.commons.csv.CSVFormat;
@@ -8,10 +9,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.math.BigDecimal;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class DataLoader {
@@ -26,17 +29,33 @@ public class DataLoader {
 
     @Scheduled(fixedRate = Integer.MAX_VALUE)
     void reloadDatabase() {
-        try (Reader in = new FileReader(String.format("%s/2018_01_report.csv", inputDataPath))) {
+        reloadFromFile(String.format("%s/2018_01_report.csv", inputDataPath));
+        reloadFromFile(String.format("%s/2018_02_report.csv", inputDataPath));
+    }
+
+    private void reloadFromFile(String file) {
+        try (Reader in = new FileReader(file)) {
             Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader().parse(in);
-            for (CSVRecord record : records) {
-                System.out.println(record.get("site"));
-                System.out.println(record.get("revenue"));
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+
+            siteMetricRepo.saveAll(StreamSupport.stream(records.spliterator(), false)
+                    .map(record -> {
+                        SiteMetric siteMetric = new SiteMetric();
+                        siteMetric.setClicks(Integer.parseInt(record.get("clicks").trim()));
+                        siteMetric.setConversions(Integer.parseInt(record.get("conversions").trim()));
+                        siteMetric.setImpressions(Integer.parseInt(record.get("impressions").trim()));
+                        siteMetric.setRequests(Integer.parseInt(record.get("requests").trim()));
+                        siteMetric.setRevenue(new BigDecimal(record.get("revenue").trim()));
+                        siteMetric.setSite(record.get("site").trim());
+                        siteMetric.setCtr(new BigDecimal(((double) siteMetric.getClicks() / siteMetric.getImpressions()) * 100));
+                        siteMetric.setCr(new BigDecimal(((double) siteMetric.getConversions() / siteMetric.getImpressions()) * 100));
+                        siteMetric.setFillRate(new BigDecimal(((double) siteMetric.getImpressions() / siteMetric.getRequests()) * 100));
+                        siteMetric.seteCpm(new BigDecimal(((double) siteMetric.getRevenue().intValue() * 1000) + siteMetric.getImpressions()));
+                        return siteMetric;
+                    }).collect(Collectors.toList()));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 
 }
